62ffc7d56d45170f8adb2b260e2a2d0797f32689
kkk <k@k>
2014-09-18 05:01:53 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+UPDATE_FILES=( \
+    ".gitconfig" \
+    ".minttyrc" \
+    ".screenrc" \
+    ".zshrc" \
+    ".tmux.config" \
+    )
+
+cd $(dirname $0)
+CUR_DIR=`pwd`
+BACKUP_DIR="TerminalSettingBackup_"$( date "+%Y%m%d%H%M%S" )
+
+mkdir ~/${BACKUP_DIR}
+for file in ${UPDATE_FILES[@]}
+do
+    if [ ! -e ~/${file} ]; then
+        mv ~/${file} ~/${BACKUP_DIR}/
+    fi
+    ln -s ${CUR_DIR}/${file} ~/${file}
+done
+
